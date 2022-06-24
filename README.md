7e1f2578636e6e3f77144b2147ff6dacc01e1913
JacobTheRanga <jacobirving.nz@gmail.com>
2022-06-24 23:19:50 +1200

    login logout system

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 13COM assessment standard
+
+passwords for accounts:
+jacob346
+frank13144
+finley8658
+jacobadmin224364745
--- app.py
+++ app.py
@@ -1,13 +1,56 @@
-from flask import Flask, render_template
+import pymysql, os, dotenv
+from passlib.hash import pbkdf2_sha256
+from flask import Flask, render_template, session, redirect, url_for, request
 app = Flask(__name__)
+app.secret_key = os.urandom(64)
+
+dotenv.load_dotenv('.env')
+
+def create_connection():
+    return pymysql.connect(
+        user = os.environ.get('USER'),
+        host = os.environ.get('HOST'),
+        password =os.environ.get('PASSWORD'),
+        db = os.environ.get('DB'),
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 @app.route('/')
 def home():
-    return render_template('/main.html')
+    if not session:
+        return redirect(url_for('login'))
+    return render_template('/layout.html')
 
-@app.route('/login')
+@app.route('/login', methods = ['get', 'post'])
 def login():
-    return render_template('/login.html')
+    if request.method != 'POST':
+        return render_template('/login.html')
+    with create_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute('select * from users where \
+                            email=%s',
+                            request.form['email']
+            )
+            userdata = cursor.fetchone()
+            if userdata is None:
+                return render_template('/login.html', output = 'There is no user with that email')
+            if pbkdf2_sha256.verify(request.form['password'], userdata['password']) is False:
+                return render_template('/login.html', output = 'Password is incorrect')
+            session['id'] = userdata['userid']
+            session['role'] = userdata['role']
+            session['user'] = str(userdata['firstname'] + ' ' +userdata['lastname'])
+            return redirect(url_for('home'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+@app.route('/register', methods = ['get', 'post'])
+def register():
+    if request.method != 'POST':
+        return render_template('/register.html')
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5555)
