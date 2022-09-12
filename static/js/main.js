b081038af7548f9b0a23dd1990b662e4b890fdb1
Jacob Irving <jacirving@student.pakuranga.school.nz>
2022-09-12 13:32:10 +1200

    replace text with input

--- static/js/main.js
+++ static/js/main.js
@@ -44,18 +44,36 @@ function addHover() {
 }
 function editSubject(id) {
     if (eval('subjectButtons'+id).getAttribute('edit') == 'False'){
-        eval('subjectButtons'+id).setAttribute('edit', 'True');
+        subjectName = eval('subjectName'+id).innerHTML;
+        subjectStartDate = eval('subjectStartDate'+id).innerHTML;
+        subjectEndDate = eval('subjectEndDate'+id).innerHTML;
         eval('subjectButtons'+id).innerHTML = `
         <button class="btn" type="submit"><i class="btn bi bi-check-circle submit" id="editSubject`+id+`" onmouseover="submitHover('editSubject`+id+`')" onmouseout="submitHover('editSubject`+id+`')"></i></button>
         <i class="btn bi bi-x-circle cancel" id="deleteSubject`+id+`" onclick="editSubject(`+id+`)" onmouseover="cancelHover('deleteSubject`+id+`')" onmouseout="cancelHover('deleteSubject`+id+`')"></i>
         `;
+        eval('subjectName'+id).innerHTML = `<div class="form-floating">
+            <input type="text" name="subjectNameInput`+id+`" class="form-control" id="subjectNameInput`+id+`" placeholder="subjectNameInput`+id+`" required value="`+subjectName+`">
+            <label for="subjectNameInput`+id+`">Subject</label>
+            </div>`;
+        eval('subjectStartDate'+id).innerHTML = `<div class="form-floating">
+            <input type="text" name="subjectStartDateInput`+id+`" class="form-control" id="subjectStartDateInput`+id+`" placeholder="subjectStartDateInput`+id+`" required value="`+subjectStartDate+`">
+            <label for="subjectStartDateInput`+id+`">Start Date</label>
+            </div>`;
+        eval('subjectEndDate'+id).innerHTML = `<div class="form-floating">
+            <input type="text" name="subjectEndDateInput`+id+`" class="form-control" id="subjectEndDateInput`+id+`" placeholder="subjectEndDateInput`+id+`" required value="`+subjectEndDate+`">
+            <label for="subjectEndDateInput`+id+`">End Date</label>
+            </div>`;
+        eval('subjectButtons'+id).setAttribute('edit', 'True');
     }
     else {
-        eval('subjectButtons'+id).setAttribute('edit', 'False');
+        eval('subjectName'+id).innerHTML = subjectName;
+        eval('subjectStart'+id).innerHTML = subjectStartDate;
+        eval('subjectEndDate'+id).innerHTML = subjectEndDate;
         eval('subjectButtons'+id).innerHTML = `
-        <i class="btn bi bi-pencil" id="editSubject`+id+`" onclick="editSubject('`+id+`')" onmouseover="editHover('editSubject`+id+`')" onmouseout="editHover('editSubject`+id+`')"></i>
-        <a href="/deleteSubject?id=`+id+`"><i class="btn bi bi-trash" id="deleteSubject`+id+`" onmouseover="deleteHover('deleteSubject`+id+`')" onmouseout="deleteHover('deleteSubject`+id+`')"></i></a>
-        `;
+            <i class="btn bi bi-pencil" id="editSubject`+id+`" onclick="editSubject('`+id+`')" onmouseover="editHover('editSubject`+id+`')" onmouseout="editHover('editSubject`+id+`')"></i>
+            <a href="/deleteSubject?id=`+id+`"><i class="btn bi bi-trash" id="deleteSubject`+id+`" onmouseover="deleteHover('deleteSubject`+id+`')" onmouseout="deleteHover('deleteSubject`+id+`')"></i></a>
+            `;
+        eval('subjectButtons'+id).setAttribute('edit', 'False');
     }
 }
 
